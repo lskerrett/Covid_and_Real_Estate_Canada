dbd20c0b4d2adf817b0c7aac5928e43b58204ee9
= <=>
2020-09-06 04:24:33 -0400

    Updated files - revision

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,9 +1,16 @@
 -- Create table
 CREATE TABLE inventory (
 	REF_DATE DATE not null,
-    City VARCHAR(25) not null,
-    Province VARCHAR(25) not null,
+    City VARCHAR(35) not null,
+    Province VARCHAR(30) not null,
 	Completed_units VARCHAR (25) not null,
     Dwelling_Type VARCHAR (25) not null,
     UNIT_VALUE int not null
+	);
+
+    CREATE TABLE price_index (
+	REF_DATE DATE not null,
+    GEO VARCHAR(60) not null,
+    Housing_Type VARCHAR(10) not null,
+	PRICE_INDEX DOUBLE not null
 	);
